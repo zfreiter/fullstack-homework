b1329b0eecb0ea6854b79537b5d1da931546feeb
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2022-10-08 23:26:47 -0700

    01,02,03
    First two should be finished, but should be rechecked.
    Third is partially finished.

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -1,7 +1,54 @@
 /** Exercise 01 - Coins **/
+const deleteMoney = (start, valueToDelete) => {
+  let count = 0;
+
+  start = start.toFixed(2);
+  while (start >= valueToDelete) {
+    start = (start - valueToDelete).toFixed(2);
+    count += 1;
+  }
+  return count;
+};
 
 const calculateChange = (input) => {
-  // Add your code here
+  if (input >= 10) {
+    return `$${input} ==> Error: the number is too large`;
+  }
+
+  const orignalInput = input;
+
+  const dollar = deleteMoney(input, 1);
+  input -= dollar;
+  const dollarStr = `${
+    dollar != 0 ? `${dollar} ${dollar > 1 ? 'dollars' : 'dollar'}` : ''
+  }`;
+
+  const quarter = deleteMoney(input, 0.25);
+  input -= quarter * 0.25;
+  const qaurterStr = `${
+    quarter != 0 ? `, ${quarter} ${quarter > 1 ? 'quarters' : 'quarter'}` : ''
+  }`;
+
+  const dime = deleteMoney(input, 0.1);
+  input -= dime * 0.1;
+  const dimeStr = `${
+    dime != 0 ? `, ${dime} ${dime > 1 ? 'dimes' : 'dime'}` : ''
+  }`;
+
+  const nickel = deleteMoney(input, 0.05);
+  input -= nickel * 0.05;
+  nickel;
+  const nickelStr = `${
+    nickel != 0 ? `, ${nickel} ${nickel > 1 ? 'nickels' : 'nickel'}` : ''
+  }`;
+
+  const penny = deleteMoney(input, 0.01);
+  const pennyStr = `${
+    penny != 0 ? `, ${penny} ${penny > 1 ? 'pennies' : 'penny'}` : ''
+  }`;
+
+  const valueToReturn = `$${orignalInput} ==> ${dollarStr}${qaurterStr}${dimeStr}${nickelStr}${pennyStr}`;
+  return valueToReturn;
 };
 
 // Sample Test Cases
--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,3 +1,35 @@
 /** Exercise 02 - Reverse **/
 
 // Add your code here
+const reverse = document.getElementById('reverse');
+const input = document.getElementById('input');
+const div = document.getElementsByTagName('div');
+let answer = document.createElement('p');
+
+answer.textContent = '';
+answer.style.marginTop = '24px';
+div[1].append(answer);
+
+const reverseNumber = (numberToReverse) => {
+  let rev = 0;
+  let digitToAdd = 0;
+
+  while (numberToReverse !== 0) {
+    digitToAdd = numberToReverse % 10;
+    rev = rev * 10 + digitToAdd;
+    numberToReverse = Math.floor(numberToReverse / 10);
+  }
+  return rev;
+};
+
+reverse.addEventListener('click', (event) => {
+  const validate = input.value.toString().length;
+
+  if (validate === 8) {
+    answer.style.color = 'green';
+    answer.textContent = `${input.value}-->${reverseNumber(input.value)}`;
+  } else {
+    answer.style.color = 'red';
+    answer.textContent = 'ERROR: Please input an 8-digit number';
+  }
+});
