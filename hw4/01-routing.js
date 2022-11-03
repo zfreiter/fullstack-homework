63fc226ec676d2044958651853fe13b00cee3c61
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2022-11-02 21:51:57 -0700

    Update 01-routing.js

--- hw4/01-routing.js
+++ hw4/01-routing.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// http://localhost:5000/welcome should return a status code 200 with a welcome message of your choice in html format
-
-// http://localhost:5000/redirect should redirect the request to '/redirected' by using 302 as the status code / the redirected page should return a redirected message of your choice
-
 // http://localhost:5000/cache should return 'this resource was cached' in html format and set the cache max age to a day
 
 // http://localhost:5000/cookie should return 'cookies… yummm' in plain text and set 'hello=world' as a cookie
@@ -40,9 +36,29 @@ app.get('/', (req, res) => {
   res.end();
 });
 
-app.get('/welcome', (req, res) => {});
-
 // Add your code here
+// http://localhost:5000/welcome should return a status code 200 with a welcome message of your choice in html format
+app.get('/welcome', (req, res) => {
+  res.status(200);
+  res.set({ 'Content-Type': 'text/html' });
+  res.send(`<h1>Hello to the welcome page!</h1>`);
+  //res.end();
+});
+
+// http://localhost:5000/redirect should redirect the request to '/redirected' by using 302 as the status code / the redirected page should return a redirected message of your choice
+app.get('/redirect', (req, res) => {
+  res.redirect(`http://localhost:${port}/redirected`);
+  //res.end();
+  // res.writeHead(302, { Location: '/redirected' });
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.write(`<h1>Redirected</h1>`);
+  // res.end();
+});
+
+app.get('/redirected', (req, res) => {
+  res.send(`<h1>You have been redirected to redirected!</h1>`);
+  res.end();
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
