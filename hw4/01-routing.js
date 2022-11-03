9c72164ac91f84e605ffa1406c865399a457afa0
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2022-11-02 23:00:38 -0700

    01 finished

--- hw4/01-routing.js
+++ hw4/01-routing.js
@@ -2,12 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// http://localhost:5000/cache should return 'this resource was cached' in html format and set the cache max age to a day
-
-// http://localhost:5000/cookie should return 'cookies… yummm' in plain text and set 'hello=world' as a cookie
-
-// For other routes, such as http://localhost:5000/other, this exercise should return a status code 404 with '404 - page not found' in html format
-
 const routes = [
   'welcome',
   'redirect',
@@ -41,32 +35,11 @@ app.get('/', (req, res) => {
 app.get('/welcome', (req, res) => {
   res.status(200);
   res.set({ 'Content-Type': 'text/html' });
+  // res.send === res.write + res.end
   res.send(`<h1>Hello to the welcome page!</h1>`);
-  //res.end();
 });
 
 // http://localhost:5000/redirect should redirect the request to '/redirected' by using 302 as the status code / the redirected page should return a redirected message of your choice
-app.get('/redirect', (req, res) => {
-  res.redirect(`http://localhost:${port}/redirected`);
-  //res.end();
-  // res.writeHead(302, { Location: '/redirected' });
-  // res.writeHead(200, { 'Content-Type': 'text/html' });
-  // res.write(`<h1>Redirected</h1>`);
-  // res.end();
-});
-
-app.get('/redirected', (req, res) => {
-  res.send(`<h1>You have been redirected to redirected!</h1>`);
-  res.end();
-});
-
-app.get('/welcome', (req, res) => {
-  res.status(200);
-  res.set({ 'Content-Type': 'text/html' });
-  res.send(`<h1>Hello to the welcome page!</h1>`);
-  res.end();
-});
-
 app.get('/redirect', (req, res) => {
   res.redirect('/redirected');
   res.end();
@@ -75,7 +48,26 @@ app.get('/redirect', (req, res) => {
 app.get('/redirected', (req, res) => {
   res.status(200);
   res.send(`<h1>You have been redirected to redirected!</h1>`);
-  res.end();
+});
+
+// http://localhost:5000/cache should return 'this resource was cached' in html format and set the cache max age to a day
+app.get('/cache', (req, res) => {
+  res.status(200);
+  res.set('Cache-control', 'max-age=86400');
+  res.send(`<h1>this resource was cached</h1>`);
+});
+
+// http://localhost:5000/cookie should return 'cookies… yummm' in plain text and set 'hello=world' as a cookie
+app.get('/cookie', (req, res) => {
+  res.status(200);
+  res.set('Set-Cookie', 'hello=world');
+  res.send(`cookies... yumm`);
+});
+
+// For other routes, such as http://localhost:5000/other, this exercise should return a status code 404 with '404 - page not found' in html format
+app.use((req, res, next) => {
+  res.status(404);
+  res.send('404 - page not found');
 });
 
 app.listen(port, () => {
