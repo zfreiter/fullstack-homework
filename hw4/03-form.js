2d0198f638f6113c98af62a48e07c03489035e75
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2022-11-04 23:18:43 -0700

    Update 03-form.js

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { appendFile } = require('fs');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,13 +7,16 @@ app.use(express.static('public'));
 
 // Use middleware urlencoded() to parse an incoming request with a urlencoded payload and return an objectß
 app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
 
 app.use('/form', express.static('public'));
 // POST request
 app.post('/submit', (req, res) => {
   // Add your code here
-  console.dir('body: %j', req.body);
-  res.end();
+  console.dir(req.body);
+  const { name, email, comment, newsletter } = req.body;
+  let data = `<div>${name}</div><div>${email}</div><div>${comment}</div><div>${newsletter}</div>`;
+  res.send(data);
 });
 
 app.listen(port, () => {
