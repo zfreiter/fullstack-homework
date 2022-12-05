da228e7f3bcb618239f815fb9ebc429afb8d0512
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2022-12-05 07:55:42 -0800

    Squashed commit of the following:
    commit 8ccfabe700703101679a1cbbb4fe16171c5a6dc7
    Author: Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
    Date:   Sun Nov 6 21:29:29 2022 -0800
    
        Update 04-templates.js
    
        Finished
    
    commit 8a19f50543819dc741f72957df535dae6237b003
    Author: Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
    Date:   Sat Nov 5 22:53:28 2022 -0700
    
        04
    
    commit 2d0198f638f6113c98af62a48e07c03489035e75
    Author: Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
    Date:   Fri Nov 4 23:18:43 2022 -0700
    
        Update 03-form.js
    
    commit bf674ae5a6dc0cdedf56a779b968385c9ba59eec
    Author: Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
    Date:   Fri Nov 4 22:51:47 2022 -0700
    
        04-form
    
    commit 9c72164ac91f84e605ffa1406c865399a457afa0
    Author: Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
    Date:   Wed Nov 2 23:00:38 2022 -0700
    
        01 finished
    
    commit 379576bdda603cf8083bf74dec6c5cfbb760874b
    Author: Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
    Date:   Wed Nov 2 21:52:43 2022 -0700
    
        Update 01-routing.js
    
    commit 63fc226ec676d2044958651853fe13b00cee3c61
    Author: Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
    Date:   Wed Nov 2 21:51:57 2022 -0700
    
        Update 01-routing.js

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -8,9 +8,19 @@ app.use(express.static('public'));
 // Use middleware urlencoded() to parse an incoming request with a urlencoded payload and return an objectß
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/form', express.static('public'));
 // POST request
-app.post('', (req, res) => {
+app.post('/submit', (req, res) => {
   // Add your code here
+  console.log(req.body);
+  const { name, email, comment, newsletter } = req.body;
+
+  let data = `<div>Name: ${name}</div><div>Email: ${email}</div><div>Comments: ${
+    comment ? comment : 'n/a'
+  }</div><div>Newsletter: ${
+    newsletter ? 'Yes, sign me up for the newsletter.' : 'No, thank you.'
+  }</div>`;
+  res.send(data);
 });
 // app.get('/', (req, res) => {
 //   res.send('hello world express');
